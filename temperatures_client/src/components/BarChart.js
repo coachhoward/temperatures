aab9c3f34e700e370d3d592fe56242202da455a1
Chris Howard <choward@ownerfinancellc.com>
2020-09-22 17:34:20 -0400

    Added additional func to charts

--- temperatures_client/src/components/BarChart.js
+++ temperatures_client/src/components/BarChart.js
@@ -8,7 +8,48 @@ const BarChart = () => {
             datasets: [
                 {
                     label: 'Avg high temps',
-                    data: []
+                    data: [],
+                    fill: false,
+                    lineTension: 0,
+        
+                    backgroundColor: "rgba(192, 77,77,.5)",
+                    borderColor: "rgba(192, 77,77,1)",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: "rgba(192, 77,77,1)",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: "rgba(192,77,77,1)",
+                    pointHoverBorderColor: "rgba(220,220,220,1)",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    spanGaps: false,
+                },
+                {
+                  label: 'Avg low temps',
+                  data:[],
+                  fill: false,
+                  lineTension: 0,
+                  backgroundColor: "rgba(75,192,192,0.4)",
+                  borderColor: "rgba(75,192,192,1)",
+                  borderCapStyle: 'butt',
+                  borderDash: [],
+                  borderDashOffset: 0.0,
+                  borderJoinStyle: 'miter',
+                  pointBorderColor: "rgba(75,192,192,1)",
+                  pointBackgroundColor: "#fff",
+                  pointBorderWidth: 1,
+                  pointHoverRadius: 5,
+                  pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                  pointHoverBorderColor: "rgba(220,220,220,1)",
+                  pointHoverBorderWidth: 2,
+                  pointRadius: 1,
+                  pointHitRadius: 10,
+                  spanGaps: false,
                 }
             ]
         }
@@ -16,6 +57,7 @@ const BarChart = () => {
         data.temperatures.forEach(temperature => {
             chartData.labels.push(temperature.month)
             chartData.datasets[0].data.push(temperature.average_high_f)
+            chartData.datasets[1].data.push(temperature.average_low_f)
         })
         return chartData
     }
@@ -23,7 +65,7 @@ const BarChart = () => {
     const createChart = (data) => {
         const ctx = document.querySelector("#temperatures");
         const tempsChart = new Chart(ctx, {
-          type: "line",
+          type: "bar",
           data: data,
         });
       };
@@ -43,7 +85,7 @@ const BarChart = () => {
 
 	return (
 	  <>
-	    <h1>Temperatures</h1>
+	    <h1>Average High and Low Temperatures</h1>
 	    <canvas id="temperatures" width="300" height="100"></canvas>
 	  </>
 	)
